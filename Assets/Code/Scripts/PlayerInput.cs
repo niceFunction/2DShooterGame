3880935af03f8cf9f7f8df3a6e173bfc08438746
Samuel Einheri <Samuel.einheri@gmail.com>
2020-09-15 17:17:53 +0200

    First commit

---
+++ Assets/Code/Scripts/PlayerInput.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SE = Samuel Einheri
+namespace SE
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerInput : MonoBehaviour
+    {
+        public float maxSpeed;
+
+        public float acceleration;
+
+        public float deceleration;
+
+        private Rigidbody2D _shipBody;
+        [SerializeField] private Vector2 _shipInput;
+        
+        private Vector2 _mousePosition;
+        private Vector2 _lookDirection;
+
+        private float _shipHorizontal;
+        private float _shipVertical;
+
+
+        private void Awake()
+        {
+            _shipBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            _shipInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            MoveShip(_shipInput);
+            RotateShip();
+        }
+
+        void MoveShip(Vector2 direction)
+        {
+            _shipBody.velocity = direction * maxSpeed;
+        }
+
+        void RotateShip()
+        {
+            //_mousePosition = Camera.main.ScreenToWorldPoint(_mousePosition);
+            _lookDirection = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(_lookDirection.y, _lookDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
+
