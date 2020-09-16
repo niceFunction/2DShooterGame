e738df523b9472065dc238760804e6397780e29c
Samuel Einheri <17195798+niceFunction@users.noreply.github.com>
2020-09-16 15:34:24 +0200

    Player can shoot, WIP on Enemy Spawner
    Player can shoot projectiles
    
    - Created enemy that follows the player
    - Started WIP on creating an enemy spawner

---
+++ Assets/Code/Scripts/Bomber.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SE
+{
+    public class Bomber : MonoBehaviour
+    {
+        public Transform playerTarget;
+        [Range(1f, 100f)]
+        public float bomberSpeed = 5f;
+
+        private Rigidbody2D _bomberBody;
+        private Vector2 _bomberMovement;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _bomberBody = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            TargetPlayer();
+        }
+
+        private void FixedUpdate()
+        {
+            MoveBomber(_bomberMovement);
+        }
+
+        private void TargetPlayer()
+        {
+            Vector3 direction = playerTarget.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            _bomberBody.rotation = angle;
+            direction.Normalize();
+            _bomberMovement = direction;
+        }
+
+        private void MoveBomber(Vector2 direction)
+        {
+            _bomberBody.MovePosition((Vector2)transform.position + 
+                (direction * bomberSpeed * Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/Code/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SE
+{
+    public class Shooting : MonoBehaviour
+    {
+        public Transform weaponPoint;
+        public GameObject bulletPrefab;
+
+        public float bulletForce = 20f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            FireWeapon();
+        }
+
+        private void FireWeapon()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                BulletFired();
+            }
+        }
+
+        private void BulletFired()
+        {
+           GameObject bullet = Instantiate(bulletPrefab, weaponPoint.position, weaponPoint.rotation);
+
+           Rigidbody2D bulletBody = bullet.GetComponent<Rigidbody2D>();
+           bulletBody.AddForce(weaponPoint.right * bulletForce, ForceMode2D.Impulse);
+        }
+    }
+}
