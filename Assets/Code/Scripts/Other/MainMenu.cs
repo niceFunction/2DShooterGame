5646fde67f8f2cb10aca1c9966cdb71d7d0e4c8a
Samuel Einheri <Samuel.einheri@gmail.com>
2020-09-25 14:52:51 +0200

    Created a simple Main Menu with instructions
    Created a simple Main Menu where a button takes the player to the game, inside the Main Menu scene, the player can find play instructions & a disclaimer about there's no input for exiting the game

---
+++ Assets/Code/Scripts/Other/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SE
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+}
